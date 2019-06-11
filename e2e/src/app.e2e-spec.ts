3a07458eae3ea255432444418bb2ab2aed7e253c
Ignacio Perez Roca <ignacio@Ignacios-MacBook-Pro.local>
2019-06-10 21:16:44 -0300

    Test Added

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to despegar-frontend!');
+    expect(page.getTitleText()).toEqual('Welcome to boldProject!');
   });
 
   afterEach(async () => {
